fc785ad759103fddd3326f81096502c573cc2209
MaryamHabib2 <maryamhabib0024@gmail.com>
2026-09-10 15:08:13 +0500

    Learnt C++ basic syntax

---
+++ C++/code.cpp
@@ -0,0 +1,11 @@
+// To print something in C++, we'll go from compilation to execution, such that, g++ code.cpp && ./a.exe  
+
+#include <iostream>
+using namespace std;
+// This is the starting point of execution of the program
+int main() {
+    
+    return 0; //always use ; at the end of each statement in C++. This is called Terminator 
+}
+
+// The above all code is basically called the "Boilerplate Code" which is going to be written in every C++ program 
